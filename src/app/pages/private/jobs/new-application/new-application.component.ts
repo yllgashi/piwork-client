20b0e2422e5b19e88145a715a36c06d546991e4f
yllgashi <yllxgashi@gmail.com>
2022-08-21 23:47:02 +0200

    implement new design for new-application component

--- src/app/pages/private/jobs/new-application/new-application.component.ts
+++ src/app/pages/private/jobs/new-application/new-application.component.ts
@@ -1,8 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { CreateApplication } from 'src/app/shared/model/create-application.model';
+import { User } from 'src/app/shared/model/user.model';
 import { ApplicationService } from 'src/app/shared/providers/application.service';
 import { DynamicComponentsService } from 'src/app/shared/providers/native/dynamic-components.service';
+import { UserService } from 'src/app/shared/providers/user.service';
 
 @Component({
   selector: 'app-new-application',
@@ -13,15 +15,22 @@ export class NewApplicationComponent implements OnInit {
   @Input('jobId') jobId: number;
   @Input('jobTitle') jobTitle: string;
   newApplicationForm: FormGroup;
+  user: User;
   isLoading: boolean;
 
   constructor(
+    private userService: UserService,
     private applicationService: ApplicationService,
     private dynamicComponentsService: DynamicComponentsService
   ) {}
 
   ngOnInit() {
+    this.fetchUser();
     this.initializeForm();
+  }
+
+  fetchUser(): void {
+    this.user = this.userService.user$.getValue();
   }
 
   initializeForm(): void {
